783fb63396b02a1d0614760acbb1bf3eb6e79abb
l-merta <lukas.merta26@gmail.com>
2025-04-15 22:00:26 +0200

    Home hero section setup

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,8 +2,15 @@
 
 const home = () => {
   return (
-    <main>
-      <h1>Mythical Academy</h1>
+    <main className="hero-main">
+      <img className="hero-bg" src="/images/bg/saf_bg.png" alt="" />
+      <div className="container">
+        <div className="text">
+          <h1>Mythical Academy</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia quisquam, illo debitis accusamus accusantium molestias corporis quibusdam pariatur odit nihil itaque tenetur delectus consectetur veniam. Maxime recusandae ducimus tempore. Laborum.</p>
+        </div>
+        <img className="hero-img" src="https://unsplash.it/2000/2000" alt="" />
+      </div>
     </main>
   )
 }
