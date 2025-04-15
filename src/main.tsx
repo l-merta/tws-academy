0abdeb58ec09c998b3e5008036cfd967e5652a7f
l-merta <lukas.merta26@gmail.com>
2025-04-15 20:45:39 +0200

    Project setup

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+import Home from './pages/home';
+
+import './styles/index.css';
+import './styles/main.css';
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <Home />
+  </StrictMode>,
+)
---
+++ src/pages/home.tsx
@@ -0,0 +1,11 @@
+//import React from 'react'
+
+const home = () => {
+  return (
+    <main>
+      <h1>Mythical Academy</h1>
+    </main>
+  )
+}
+
+export default home
