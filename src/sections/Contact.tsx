4dee2c77ab58086033b0968d657678e4c1a29a33
l-merta <lukas.merta26@gmail.com>
2025-04-20 20:27:50 +0200

    Contact section text upd

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -11,7 +11,7 @@ const Contact = () => {
           <ul>
             <li><ContactInfo icon='envelope' link='mailto:info@mytickaAkademie.cz'>info@mytickaAkademie.cz</ContactInfo></li>
             <li><ContactInfo icon='phone' link='tel:+420133233789'>+420 133 233 789</ContactInfo></li>
-            <li><ContactInfo icon='location-dot' link='https://maps.app.goo.gl/XT1Zhiqr1TFi7R9o7'>Jaselská 826, 280 90 Kolín 2</ContactInfo></li>
+            <li><ContactInfo icon='location-dot' link='https://maps.app.goo.gl/XT1Zhiqr1TFi7R9o7'>Jaselská 826, 280 02 Kolín 2</ContactInfo></li>
           </ul>
         </div>
         <div className="map-container">
